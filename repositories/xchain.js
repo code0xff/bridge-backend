4d0506a4d0f3ff03ea367b8e862068a5469b80a1
code0xff <ian.jungyong.um@gmail.com>
2022-01-20 17:24:48 +0900

    fix: initial xchain evaluation data row created when xchain created

--- repositories/xchain.js
+++ repositories/xchain.js
@@ -1,9 +1,17 @@
 const pool = require('../db/pool')
+const { v4: uuidv4 } = require('uuid');
 
 const createXchain = async (name, enName, image, detail) => {
   const conn = await pool.getConnection(async conn => conn)
-  await conn.query(`insert into xchain.xchain (xchain_name, xchain_en_name, xchain_image, xchain_detail, created_at, updated_at, created_by, updated_by) 
-  values(?, ?, ?, ?, now(), now(), 'admin', 'admin')`, [name, enName, image, detail])
+  await conn.beginTransaction()
+  const xchainId = uuidv4()
+  await conn.query(`insert into xchain.xchain (xchain_id, xchain_name, xchain_en_name, xchain_image, xchain_detail, created_at, updated_at, created_by, updated_by) 
+  values(?, ?, ?, ?, ?, now(), now(), 'admin', 'admin')`, [xchainId, name, enName, image, detail])
+  await conn.query(
+    `insert into xchain.xchain_eval (
+    xchain_id, dec_score, dec_detail, per_score, per_detail, sec_score, sec_detail, scal_score, scal_detail,
+    created_at, updated_at, created_by, updated_by) values (?, 0, '', 0, '', 0, '', 0, '', now(), now(), 'admin', 'admin')`, [xchainId])
+  await conn.commit() 
 }
 
 const findXchain = async (from, to) => {
