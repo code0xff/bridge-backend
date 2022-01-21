5df1d809ebcd3a95f3590bd651f766bac5066d0c
code0xff <ian.jungyong.um@gmail.com>
2022-01-22 00:18:47 +0900

    fix: add mysql connection release

--- repositories/evaluation.js
+++ repositories/evaluation.js
@@ -1,18 +1,31 @@
 const pool = require('../db/pool')
 
 const updateEvaluation = async (eval) => {
-    const conn = await pool.getConnection(async conn => conn)
-    console.log(eval)
-    await conn.query(
-        `update xchain.xchain_eval set dec_score=?, dec_detail=?, per_score=?, per_detail=?, sec_score=?, sec_detail=?, scal_score=?, scal_detail=?,
+    let conn
+    try {
+        conn = await pool.getConnection(async conn => conn)
+        await conn.query(
+          `update xchain.xchain_eval set dec_score=?, dec_detail=?, per_score=?, per_detail=?, sec_score=?, sec_detail=?, scal_score=?, scal_detail=?,
         updated_at=now(), updated_by='admin' where xchain_id=?`,
-        [eval.decScore, eval.decDetail, eval.perScore, eval.perDetail, eval.secScore, eval.secDetail, eval.scalScore, eval.scalDetail, eval.xchainId])
+          [eval.decScore, eval.decDetail, eval.perScore, eval.perDetail, eval.secScore, eval.secDetail, eval.scalScore, eval.scalDetail, eval.xchainId])
+    } catch (e) {
+        throw e
+    } finally {
+        conn.release()
+    }
 }
 
 const getEvaluation = async (xchainId) => {
-    const conn = await pool.getConnection(async conn => conn)
-    const rows = await conn.query(`select * from xchain.xchain_eval where xchain_id = ?`, [xchainId])
-    return rows[0]
+    let conn
+    try {
+        conn = await pool.getConnection(async conn => conn)
+        const rows = await conn.query(`select * from xchain.xchain_eval where xchain_id = ?`, [xchainId])
+        return rows[0]
+    } catch (e) {
+      throw e
+    } finally {
+      conn.release()
+    }
 }
 
 module.exports = {
--- repositories/xchain.js
+++ repositories/xchain.js
@@ -2,34 +2,57 @@ const pool = require('../db/pool')
 const { v4: uuidv4 } = require('uuid');
 
 const createXchain = async (xchain) => {
-  const conn = await pool.getConnection(async conn => conn)
-  await conn.beginTransaction()
-  const xchainId = uuidv4()
-  await conn.query(`insert into xchain.xchain (xchain_id, xchain_name, xchain_en_name, xchain_image, xchain_detail, created_at, updated_at, created_by, updated_by) 
+  let conn
+  try {
+    conn = await pool.getConnection(async conn => conn)
+    await conn.beginTransaction()
+    const xchainId = uuidv4()
+    await conn.query(`insert into xchain.xchain (xchain_id, xchain_name, xchain_en_name, xchain_image, xchain_detail, created_at, updated_at, created_by, updated_by) 
     values(?, ?, ?, ?, ?, now(), now(), 'admin', 'admin')`, [xchainId, xchain.xchainName, xchain.xchainEnName, xchain.xchainImage, xchain.xchainDetail])
-  await conn.query(
-    `insert into xchain.xchain_eval (
+    await conn.query(
+      `insert into xchain.xchain_eval (
       xchain_id, dec_score, dec_detail, per_score, per_detail, sec_score, sec_detail, scal_score, scal_detail,
       created_at, updated_at, created_by, updated_by) values (?, 0, '', 0, '', 0, '', 0, '', now(), now(), 'admin', 'admin')`, [xchainId])
-  await conn.query(
-    `insert into xchain.xchain_feedback (
+    await conn.query(
+      `insert into xchain.xchain_feedback (
         fee_sum, time_sum, ui_sum, support_sum, feedback_count, xchain_id,
         created_at, updated_at, created_by, updated_by) values (0, 0, 0, 0, 0, ?, now(), now(), 'admin', 'admin')`, [xchainId])
-  await conn.commit() 
+    await conn.commit()
+  } catch(e) {
+    throw e
+  } finally {
+    conn.release()
+  }
 }
 
 const findAllXchain = async () => {
-  const conn = await pool.getConnection(async conn => conn)
-  const rows = await conn.query("select * from xchain.xchain order by 1", [])
-  console.log(rows[0])
-  return rows[0]
+  let conn
+  try {
+    conn = await pool.getConnection(async conn => conn)
+    const rows = await conn.query("select * from xchain.xchain order by 1", [])
+    console.log(rows[0])
+    conn.release()
+    return rows[0]
+  } catch (e) {
+    throw e
+  } finally {
+    conn.release()
+  }
 }
 
 const findXchain = async (xchainId) => {
-  const conn = await pool.getConnection(async conn => conn)
-  const rows = await conn.query("select * from xchain.xchain where xchain_id = ?", [xchainId])
-  console.log(rows[0])
-  return rows[0]
+  let conn
+  try {
+    conn = await pool.getConnection(async conn => conn)
+    const rows = await conn.query("select * from xchain.xchain where xchain_id = ?", [xchainId])
+    console.log(rows[0])
+    conn.release()
+    return rows[0]
+  } catch (e) {
+    throw e
+  } finally {
+    conn.release()
+  }
 }
 
 
