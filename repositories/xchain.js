ff04fca1a36a8ea6f030da1a7e99059f5477c709
code0xff <ian.jungyong.um@gmail.com>
2022-01-23 11:41:14 +0900

    fix: remove duplicated connection release

--- repositories/xchain.js
+++ repositories/xchain.js
@@ -30,7 +30,6 @@ const findAllXchain = async () => {
   try {
     conn = await pool.getConnection(async conn => conn)
     const rows = await conn.query("select * from xchain.xchain order by 1", [])
-    conn.release()
     return rows[0]
   } catch (e) {
     throw e
@@ -44,7 +43,6 @@ const findXchain = async (xchainId) => {
   try {
     conn = await pool.getConnection(async conn => conn)
     const rows = await conn.query("select * from xchain.xchain where xchain_id = ?", [xchainId])
-    conn.release()
     return rows[0]
   } catch (e) {
     throw e
